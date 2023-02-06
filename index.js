980a3928558a8391ff336f0c67ffa1faea14e69a
Cody Boring <codyboring94@gmail.com>
2023-02-06 13:39:23 -0800

    Finished looping assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const gifts = ["Charlie", "Samip", "Ali"];
+const Event = "birthday"
+
+function writeCards(gifts, Event) {
+   let writeCardsEmptyArray = [...gifts]
+    for (let i = 0;i < writeCardsEmptyArray.length ; i++) {
+        writeCardsEmptyArray[i] = `Thank you, ${gifts[i]}, for the wonderful ${Event} gift!`;
+        
+    }
+    return writeCardsEmptyArray;
+  
+}
+
+writeCards(gifts, Event)
+
+function countDown(assignment) {
+    let count = assignment
+    while (count >= 0)
+    console.log(count--)
+}
+
+countDown(10)
